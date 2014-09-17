1e1328586a98eef1d6ac719f5f642eff9f7c8202
Владимир <vkharseev@gmail.com>
2014-09-17 17:34:13 +0400

    Внести изменения в логовор

--- application/views/physical_interface/contracts/new-contract.php
+++ application/views/physical_interface/contracts/new-contract.php
@@ -184,7 +184,7 @@
     </tbody>
 </table>
 </p>
-<p class="page-break"> </p>
+<p class="page-break">&nbsp;</p>
 <p class="center title">
     <strong>Приложение №1 к договору № <?=number_order($order['number'],$order['year']);?></strong>
 </p>
